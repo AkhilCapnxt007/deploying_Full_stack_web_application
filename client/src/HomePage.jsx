e364c9cd8fd46a859dfd538601b2e0f8f18a033b
Akhil Nayak <aakhiljay06@gmail.com>
2023-05-05 14:02:57 +0530

    changes done

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -6,7 +6,7 @@ import car from "./images/car4.jpg";
 function FetchRequest() {
   return (
     <>
-      <h1> The App is Listening Successfull On Ec2 instance Congrats ...</h1>
+      <h1>Changed The App is Listening Successfull On Ec2 instance Congrats ...</h1>
       <img src={car} alt="" />
     </>
   );
